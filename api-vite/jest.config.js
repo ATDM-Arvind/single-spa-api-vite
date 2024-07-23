012c35f0a087f4467582264b527990b15a8dceab
Arvind <arvind.kumar@accolitedigital.com>
2024-07-23 17:02:32 +0530

    added sample unit testing

---
+++ api-vite/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
